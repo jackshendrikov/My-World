e3e1eb27d897082883fc35209c87aeeff108b192
Jack Shendrikov <jackshendrikov@gmail.com>
2021-04-22 01:02:57 +0300

    Update urls

--- ip-locator/apps.py
+++ ip-locator/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class IpLocatorConfig(AppConfig):
     name = 'ip-locator'
+    label = 'iplocator'
--- ip-locator/urls.py
+++ ip-locator/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'ip-locator'
 urlpatterns = [
-    path("", views.index)
+    path('', views.index)
 ]
