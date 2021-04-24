7d45628dc002b65f67abb6ecb98f1d4f6d94a6f9
Jack Shendrikov <jackshendrikov@gmail.com>
2021-04-24 15:25:25 +0300

    Add whitenoise for staticfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -175,6 +175,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
