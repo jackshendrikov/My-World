342c6bff79962fdbf581007b0dbd5e71d889d20e
Jack Shendrikov <jackshendrikov@gmail.com>
2021-09-09 12:08:07 +0300

    Add field timestamp to myrating (movie-finder)

--- users/models.py
+++ users/models.py
@@ -10,9 +10,16 @@ class MyRating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     rating = models.IntegerField(default=5, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    timestamp = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return '{0}: {1}({2})'.format(self.user.username, self.movie.title, self.rating)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.timestamp = timezone.now()
+        self.timestamp = timezone.now()
+        return super(MyRating, self).save(*args, **kwargs)
 
 
 class Review(models.Model):
