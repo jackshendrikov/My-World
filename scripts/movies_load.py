b3c12f905ef193acd831b4a8aabb552791196a44
Jack Shendrikov <jackshendrikov@gmail.com>
2021-12-02 22:54:14 +0200

    Update movies_load script

--- scripts/movies_load.py
+++ scripts/movies_load.py
@@ -72,7 +72,7 @@ def create_movie_record(movie_id: str) -> Optional[MovieInfo]:
 
     data = json.loads(json_data)
 
-    if "Error" not in data.keys() and "N/A" not in data.values():
+    if "Error" not in data.keys() and data["imdbRating"] != "N/A" and data["imdbVotes"] != "N/A":
         return MovieInfo(
             imdb_id=data["imdbID"],
             title=data["Title"],
