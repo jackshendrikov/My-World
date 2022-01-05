847b8786050d5c09968edc18729a720be45ca581
Jack Shendrikov <jackshendrikov@gmail.com>
2022-01-05 10:27:08 +0200

    Fix update movies script

--- scripts/update_movies.py
+++ scripts/update_movies.py
@@ -1,7 +1,7 @@
 import json
 import urllib.request
 
-from movie_finder.models import Movie
+from movie_finder.models import Movie, Rate
 from jackshen.settings import OMDB_API, OMDB_API_2, OMDB_LINK
 from home.utils.check_progress import print_progress
 
@@ -23,7 +23,7 @@ for i, item in enumerate(Movie.objects.order_by('-release').iterator()):
     data = json.loads(json_data)
 
     item.votes = data["imdbVotes"].replace(",", "")
-    item.rating.rating = data["imdbRating"]
+    item.rating = Rate.objects.get(rating=data["imdbRating"])
     item.save()
     print_progress(i + 1, Movie.objects.count(), prefix='Progress:', suffix='Complete', length=50)
 
